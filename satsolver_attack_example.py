f95805d985d4bdc9415150c6ca239d2e82b5f765
christianmartindale <cam119@duke.edu>
2017-06-14 14:41:26 -0400

    paper intro, example, sat solver program started

---
+++ satsolver_attack_example.py
@@ -0,0 +1,30 @@
+import pycosat
+import itertools
+
+# Program to simulate a Database Reconstruction Attack on databases
+# of various sizes
+# Uses pycosat - Python wrapper of the picoSAT SAT solver (in C)
+# @author Christian Martindale
+
+# original CNF eqn is (A OR B) AND (NOT(B) OR C OR (NOT(D)))
+# AND (D OR (NOT(E)))
+
+#translated into DIMACS CNF format:
+
+# p CNF 5 3
+# 1 2 0
+# -2 3 -4 0
+# 4 -5 0
+
+#pycosat input: List[List], ignore 0s
+myCNF = [[1,2], [-2,3,-4], [4,-5]]
+
+#only yields the first solution found, if satisfiable
+ans = pycosat.solve(myCNF)
+#yields as many solutions as it finds until iterator is exhausted
+allAns = pycosat.itersolve(myCNF)
+print("First solution found: " , ans)
+print("All solutions found: ")
+for solution in allAns:
+    print(solution)
+print("Total number of solutions: " , len(list(pycosat.itersolve(myCNF))))
--- solve.py
+++ solve.py
@@ -3,3 +3,17 @@
 # Quick test to see how long it takes to solve.
 #
 
+# use SAT solver? SMT solver (find python implementation)? or just simultaneous eqn solver (for small size)?
+
+# need to know:
+# format of input datafile
+# how to encode each non-boolean variables into boolean
+
+# Structure
+# read file
+# extract data into List for processing, where each entry represents a 'person'
+# synthesize the preliminary constraint equations by extracting the corresponding data from each person into Lists
+# Form the actual equations (floating point rounding transformations, etc.)
+
+def kappafunction():
+    print("Kappa hahaa")
