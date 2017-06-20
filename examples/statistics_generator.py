4ef45a48d84f6b08f2611f1e53136532d1c27975
Christian Martindale <chmartindale@gmail.com>
2017-06-20 15:04:12 +0000

    more statistics functions and filtering capabilities, tested on example datafile

--- examples/statistics_generator.py
+++ examples/statistics_generator.py
@@ -4,7 +4,7 @@ import example_generator
 
 #reads in a file of PII data and generates statistics for mock publication
 #input format:
-    # 1 CATEGORIES
+    # 1 categories (in order)
     # 2-N each line represents a person, data values are ints separated by spaces
 
 #Categories (update as new categories are added):
@@ -16,6 +16,19 @@ def read_file(filename):
         lines = [line.rstrip() for line in f if not line.isspace()]
         return lines[1:]
 
+#just for readability's sake later
+def count_people(data):
+    return len(data)
+
+#creates a list with only people matching the filter criteria in it
+def filter_data(data, param_name, value):
+    filtered = []
+    for line in data:
+        person = line.split()
+        if int(person[categories[param_name]]) == value:
+            filtered.append(line)
+    return filtered
+
 #counts the number of people in the file matching a description
 def count_matches(data, param_name, value):
     ans = 0
@@ -25,10 +38,28 @@ def count_matches(data, param_name, value):
             ans += 1
     return ans
 
+#calculates the percent of people matching criteria 2 that also match 
+#criteria 1
+def calc_percent(data, param_name_1, value_1, param_name_2, value_2):
+    numerator = count_matches(param_name_1, value_1)
+    denominator = count_matches(param_name_2, value_2)
+    return numerator / denominator
+    
+#calculates the average value of a trait in a certain population
+def calc_avg(filtered_data, param_name):
+    population_size = len(filtered_data)
+    running_total = 0
+    for line in filtered_data:
+        person = line.split()
+        running_total += int(person[categories[param_name]])
+    return running_total / population_size
+
 if __name__ == '__main__':
     filename ="example_survey_responses.txt"
     data = read_file(filename)
     param_name = "race"
-    value = 1
-    count = count_matches(data, param_name, value)
-    print("Number of", param_name, "with value", value, "is", count)
+    race_value = 1
+    count = count_matches(data, param_name, race_value)
+    print("Number of people with", param_name, "value", race_value, "is", count)
+    filtered_whites_only = filter_data(data, "race", 1)
+    print("Average age of whites is", calc_avg(filtered_whites_only, "age"))
