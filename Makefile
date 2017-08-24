d66297a73ae2d7f50fca362e265edfa0b1bf9436
Simson Garfinkel <simsong@acm.org>
2017-08-23 20:55:05 -0400

    update

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@ all: vars.tex constraints_.cnf
 	latexmk -pdf white_paper.tex
 
 constraints_.cnf: constraints.csp
-	perl sugar-v2-3-2/bin/sugar -jar sugar-v2-3-2/bin/sugar-v2-3-2.jar -keep -tmp constraints_ -output constraints.out constraints.csp 
+	perl sugar-v2-3-2/bin/sugar -jar sugar-v2-3-2/bin/sugar-v2-3-2.jar \
+	     -keep -tmp constraints_ -output constraints.out constraints.csp > sugar.out
+
 
 vars.tex: constraints_.cnf make_vars.py
 	python make_vars.py 
--- make_vars.py
+++ make_vars.py
@@ -14,6 +14,23 @@ def dimacs():
             
 
 if __name__=="__main__":
+    # Get the varaibles out of constraints.csp
+    # Create the solved table
+    vars = {}
+    with open("sugar.out","r") as f:
+        for line in f:
+            line = line.strip()
+            if line[0]=='s' and line!='s SATISFIABLE':
+                raise RuntimeError("Constarints not satisfied")
+            if line[0]=='a' and "\t" in line:
+                (var,val) = line[2:].split("\t")
+                vars[var] = val
+
+    with open("id_table.tex","r") as fin:
+        with open("id_table_solved.tex","w") as fout:
+            for line in fin:
+                fout.write(" ".join([vars.get(var,var) for var in line.split(" ")]))
+                    
     with open("vars.tex","w") as f:
         print("\\newcommand\\NumConstraintLines{{ {} }}".format(linecount("constraints.csp",lambda a:";" not in a)),file=f)
         print("\\newcommand\\NumSExpressions{{ {} }}".format(linecount("constraints.csp",lambda a:a.startswith("("))),file=f)
@@ -22,3 +39,4 @@ if __name__=="__main__":
         print("\\newcommand\\NumClauses{{ {:,d} }}".format(clauses),file=f)
         print("\\newcommand\\NumDIMACSLines{{ {:,d} }}".format(linecount("constraints_.cnf",lambda a:True)),file=f)
 
+        
