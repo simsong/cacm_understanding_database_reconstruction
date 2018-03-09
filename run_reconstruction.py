2726267c6acf1354f8e30a9daf9a9db6c05e2b1c
Simson Garfinkel <simsong@acm.org>
2018-03-09 11:08:48 -0500

    better control over running sugar

--- run_reconstruction.py
+++ run_reconstruction.py
@@ -7,7 +7,13 @@
 SOLVER="picosat"
 
 import sys
-import subprocess
+from subprocess import Popen,PIPE,call
+
+JARFILE='./sugar-v2-3-2/bin/sugar-v2-3-2.jar'
+# Our encodings
+SEXMAP = {"1":"M", "0":"F"}
+RACEMAP = {"1":"W", "0":"B"}
+MARRIAGEMAP = {"0":"S", "1":"M"}
 
 def linecount(fname,filter_func):
     return len(list(filter(filter_func,open(fname,"r").read().split("\n"))))
@@ -20,80 +26,41 @@ def tally_dimacs_file(dimacs_file):
             assert fields[0]=='p'
             assert fields[1]=='cnf'
             return (int(fields[2]),int(fields[3]))
-def reverse_map(m):
-    return {v: k for k, v in m.items()}
+
+def unmap(var):
+    # Given a variable that is to be unmapped (e.g. A2)
+    # transform it to text
+    try:
+        rvar = vars[var]        # rvar is what we are returning
+    except KeyError:
+        return var
+    if var[0]=='S':
+        return SEXMAP[rvar]
+    if var[0]=='R':
+        return RACEMAP[rvar]
+    if var[0]=='M':
+        return MARRIAGEMAP[rvar]
+    # Must be an age
+    return rvar
 
 def latex_def(name,value):
     return "\\newcommand\\{}{{\\xspace{:,d}\\xspace}}\n".format(name,value)
 
+def decode_picosat_out(outdata,mapfile):
+    cmd = ['java','-cp',JARFILE,'jp.kobe_u.sugar.SugarMain','-decode','/dev/stdin',mapfile]
+    (out,err) = Popen(cmd,stdin=PIPE,stdout=PIPE,stderr=PIPE).communicate(outdata.encode('utf-8'))
+    out = out.decode('utf-8')
+    err = err.decode('utf-8')
+    if err:
+        raise RuntimeError("sugar decode error: "+err)
+    return out
 
-sexmap = {"1":"M", "0":"F"}
-rsexmap = reverse_map(sexmap)
-racemap = {"1":"W", "0":"B"}
-rracemap = reverse_map(racemap)
-marriagemap = {"0":"S", "1":"M"}
-rmarraigemap = reverse_map(marriagemap)
-
-if __name__=="__main__":
-    from argparse import ArgumentParser,ArgumentDefaultsHelpFormatter
-    parser = ArgumentParser( formatter_class = ArgumentDefaultsHelpFormatter )
-    parser.add_argument("--seed",type=int,default=0)
-    parser.add_argument("--problem",default="one-block.csp")
-    args = parser.parse_args()
-    args.sugar_output = "constraints.sugar.out"
-    args.sugar_cnf    = "constraints_.cnf"
-
-    solver_with_seed="{} -s {} --all".format(SOLVER,args.seed)
-
-    # Run the pre-processor and remove the files that begin with a '#'
-    cmd = ['cpp',args.problem]
-    (out,err) = subprocess.Popen(cmd,stdout=subprocess.PIPE).communicate()
-    with open("constraints.csp","w") as f:
-        for line in out.decode('utf-8').split("\n"):
-            if line.startswith('#'):
-                continue
-            f.write(line)
-            f.write("\n")
-
-    # Run sugar, which runs the solver
-    cmd = ['perl','sugar-v2-3-2/bin/sugar',
-           '-jar','sugar-v2-3-2/bin/sugar-v2-3-2.jar',
-           '-solver',solver_with_seed,'-keep','-tmp','constraints_',
-           '-output','constraints.out','constraints.csp']
-    print(" ".join(cmd))
-    r = subprocess.call(cmd,stdout=open(args.sugar_output,"w"))
-    if r!=0:
-        raise RuntimeError("sugar failed???")
-
-    # Get the varaibles out of constraints.csp
-    # Create the solved table
-
-    cout = open(args.sugar_output,"r").read()
-    if "ERROR" in cout:
-        print("Error in running sugar:")
-        print(cout)
-        exit(1)
-    
-
+def extract_vars(outdata):
+    """Extract the variables from the sugar output."""
     # vars[] is the mapping of the sugar variable to the solution
     vars = {}
-    def unmap(var):
-        # Given a variable that is to be unmapped (e.g. A2)
-        # transform it to text
-        try:
-            rvar = vars[var]        # rvar is what we are returning
-        except KeyError:
-            return var
-        if var[0]=='S':
-            return sexmap[rvar]
-        if var[0]=='R':
-            return racemap[rvar]
-        if var[0]=='M':
-            return marriagemap[rvar]
-        # Must be an age
-        return rvar
 
-    for line in cout.split("\n"):
+    for line in outdata.split("\n"):
         line = line.strip()
         if len(line)==0: continue
         if line[0]=='s' and line!='s SATISFIABLE':
@@ -101,8 +68,10 @@ if __name__=="__main__":
         if line[0]=='a' and "\t" in line:
             (var,val) = line[2:].split("\t")
             vars[var] = val
+    return vars
 
-    # Print information about reconstruction, sorted
+def vars_to_codes(vars):
+    """Transform the variables to a list of (age,code) person codes"""
     results = []
     for i in range(1,100):
         si = str(i)
@@ -111,10 +80,102 @@ if __name__=="__main__":
         except KeyError:
             break
         desc = "{:>2}{}{}{}".format(age,
-                                 sexmap[vars["S"+si]],
-                                 racemap[vars["R"+si]],
-                                 marriagemap[vars["M"+si]])
+                                 SEXMAP[vars["S"+si]],
+                                 RACEMAP[vars["R"+si]],
+                                 MARRIAGEMAP[vars["M"+si]])
         results.append((int(age),desc))
+    results.sort()
+    return results
+
+if __name__=="__main__":
+    from argparse import ArgumentParser,ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser( formatter_class = ArgumentDefaultsHelpFormatter,
+                             description="Runs sugar with picosat and parse the results into LaTeX")
+    parser.add_argument("--seed",type=int,default=0,help="specify the seed for the solver")
+    parser.add_argument("--problem",default="one-block.csp",help="specify the problem CSP")
+    parser.add_argument("--cnf",default="constraints.cnf",help="specify cnf file")
+    parser.add_argument("--parseout",help="Parse the variables out of an .out file. A map file must be specified")
+    parser.add_argument("--parseall",help='Evaluate output file of the picosat --all')
+    parser.add_argument("--out",help='sugar output file',default='constraints.out')
+    parser.add_argument("--map",help="Specify map file",default='constraints.map')
+    parser.add_argument("--all",help="Compute all possible solutions",action="store_true")
+    args = parser.parse_args()
+    args.sugar_output = "constraints.sugar.out"
+
+    def parse_and_print(out):
+        res = decode_picosat_out(out,args.map)
+        vars = extract_vars(res)
+        for (age,code) in sorted(vars_to_codes(vars)):
+            print(code)
+
+    if args.parseout:
+        out = open(args.parseout,"r").read()
+        parse_and_print(out)
+        exit(1)
+
+    if args.parseall:
+        out = ""
+        linenumber = 0
+        solution = 0
+        for line in open(args.parseall,"r"):
+            linenumber += 1
+            out += line
+            if line=="s SATISFIABLE\n" and linenumber>1:
+                solution += 1
+                print("Solution:",solution)
+                parse_and_print(out)
+                out = ""
+                continue
+        # Handle the last
+        solution += 1
+        print("Solution:",solution)
+        parse_and_print(out)
+        exit(1)
+        
+
+    # Run the pre-processor and remove the files that begin with a '#'
+    cmd = ['cpp',args.problem]
+    (out,err) = Popen(cmd,stdout=PIPE).communicate()
+    with open("constraints.csp","w") as f:
+        for line in out.decode('utf-8').split("\n"):
+            if line.startswith('#'):
+                continue
+            f.write(line)
+            f.write("\n")
+
+    # Run sugar, which runs the solver
+    all = "" if not args.all else " --all"
+    solver_cmd="{} -s {}{}".format(SOLVER,args.seed,all)
+    tmp = args.cnf.replace(".cnf","")
+    cmd = ['perl','sugar-v2-3-2/bin/sugar',
+           '-jar','sugar-v2-3-2/bin/sugar-v2-3-2.jar',
+           '-solver',solver_cmd,'-keep','-tmp','constraints',
+           '-output','constraints.out','constraints.csp']
+    print(" ".join(cmd))
+    p = Popen(cmd,stdout=PIPE,stderr=PIPE)
+    (out,err) = p.communicate()
+    sugar_out = out.decode('utf-8')
+    sugar_err = err.decode('utf-8')
+    if p.returncode!=0:
+        raise RuntimeError("sugar returned {}. out='{}' err='{}'".format(p.returncode,sugar_out,sugar_err))
+        exit(1)
+
+    # keep a copy of the sugar output
+    with open(args.sugar_output,'w') as f:
+        f.write(sugar_out)
+
+    # Now constraints.out has the output from picosat
+    # args.sugar_output has the output from sugar
+
+    if "ERROR" in sugar_out:
+        print("Error in running sugar:")
+        print(sugar_out)
+        exit(1)
+    
+    vars    = extract_vars(sugar_out)
+    results = vars_to_codes(vars)
+
+    # Print information about reconstruction, sorted
     print("Seed: {}".format(args.seed))
     for (age,desc) in sorted(results):
         print(desc)
@@ -132,7 +193,7 @@ if __name__=="__main__":
         print(latex_def("NumSExpressions",
                         linecount("constraints.csp",lambda a:a.startswith("("))),
               file=f)
-        (variables,clauses) = tally_dimacs_file(args.sugar_cnf)
+        (variables,clauses) = tally_dimacs_file(args.cnf)
         print(latex_def("NumVariables",variables),file=f)
         print(latex_def("NumClauses",clauses),file=f)
-        print(latex_def("NumDIMACSLines", linecount(args.sugar_cnf,lambda a:True)),file=f)
+        print(latex_def("NumDIMACSLines", linecount(args.cnf,lambda a:True)),file=f)
